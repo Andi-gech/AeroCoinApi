4f46de44b44c4e1d91a2817072487ec02d840f36
Andi-gech <andi.fab23@gmail.com>
2024-06-16 10:21:22 -0400

    sd

--- connect.js
+++ connect.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 
 const connect=()=>{
-    return mongoose.connect('mongodb+srv://andifab23:uQ91DtVyt1wKdfYp@cluster0.bnkfsl8.mongodb.net/')
+    return mongoose.connect('mongodb+srv://andifab23:uQ91DtVyt1wKdfYp@cluster0.bnkfsl8.mongodb.net/',)
 }
 module.exports=connect
